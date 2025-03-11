312459b2c223a47fee9d40741e5962f41c5a7867
LuisAugusto2005 <luisaugustomarquescosta@gmail.com>
2025-03-11 12:02:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var button, amt, container;
 const labels = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
                   "P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-const operators = ["C","DEL","(",")","->","^","<->","v","~","v","V","F","="];
+const operators = ["C","DEL","(",")","→","^","↔","v","~","v","V","F","="];
 
 var operation = [];
 
